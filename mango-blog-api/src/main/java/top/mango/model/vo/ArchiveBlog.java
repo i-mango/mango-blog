a46a1d29a30dbce92bddfed7f097aed6c7c18a8f
潘纪婷 <admin@manggo.cn>
2023-08-26 21:51:45 +0800

    添加ArchiveBlog.java文件

---
+++ mango-blog-api/src/main/java/top/mango/model/vo/ArchiveBlog.java
@@ -0,0 +1,23 @@
+package top.mango.model.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @Description: 归档页面博客简要信息
+ * @Author: Mango
+ * @Date: 2023-08-07
+ */
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ArchiveBlog {
+	private Long id;
+	private String title;
+	private String day;
+	private String password;
+	private Boolean privacy;
+}
