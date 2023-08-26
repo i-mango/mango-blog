49c0ba6c0446f557dc53913e215440b06755f24b
潘纪婷 <admin@manggo.cn>
2023-08-26 21:53:57 +0800

    添加Copyright.java文件

---
+++ mango-blog-api/src/main/java/top/mango/model/vo/Copyright.java
@@ -0,0 +1,20 @@
+package top.mango.model.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @Description: copyright
+ * @Author: Mango
+ * @Date: 2023-08-07
+ */
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Copyright {
+	private String title;
+	private String siteName;
+}
