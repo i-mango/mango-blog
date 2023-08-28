72108b1793402b86db234f970b3c6cf8915d17bd
潘纪婷 <admin@manggo.cn>
2023-08-28 08:39:39 +0800

    添加Friend.java文件

---
+++ mango-blog-api/src/main/java/top/mango/model/vo/Friend.java
@@ -0,0 +1,22 @@
+package top.mango.model.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @Description: 友链VO
+ * @Author: Mango
+ * @Date: 2023-08-07
+ */
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Friend {
+	private String nickname;//昵称
+	private String description;//描述
+	private String website;//站点
+	private String avatar;//头像
+}
