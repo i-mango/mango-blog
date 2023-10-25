23f088ff7531eeba44c6aaa15a75aae0bdba5679
潘纪婷 <admin@manggo.cn>
2023-10-25 16:39:33 +0800

    添加BlogService.java文件

---
+++ mango-blog-api/src/main/java/top/mango/service/BlogService.java
@@ -0,0 +1,71 @@
+package top.mango.service;
+
+import top.mango.entity.Blog;
+import top.mango.model.dto.BlogVisibility;
+import top.mango.model.vo.BlogDetail;
+import top.mango.model.vo.BlogInfo;
+import top.mango.model.vo.NewBlog;
+import top.mango.model.vo.PageResult;
+import top.mango.model.vo.RandomBlog;
+import top.mango.model.vo.SearchBlog;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BlogService {
+	List<Blog> getListByTitleAndCategoryId(String title, Integer categoryId);
+
+	List<SearchBlog> getSearchBlogListByQueryAndIsPublished(String query);
+
+	List<Blog> getIdAndTitleList();
+
+	List<NewBlog> getNewBlogListByIsPublished();
+
+	PageResult<BlogInfo> getBlogInfoListByIsPublished(Integer pageNum);
+
+	PageResult<BlogInfo> getBlogInfoListByCategoryNameAndIsPublished(String categoryName, Integer pageNum);
+
+	PageResult<BlogInfo> getBlogInfoListByTagNameAndIsPublished(String tagName, Integer pageNum);
+
+	Map<String, Object> getArchiveBlogAndCountByIsPublished();
+
+	List<RandomBlog> getRandomBlogListByLimitNumAndIsPublishedAndIsRecommend();
+
+	void deleteBlogById(Long id);
+
+	void deleteBlogTagByBlogId(Long blogId);
+
+	void saveBlog(top.mango.model.dto.Blog blog);
+
+	void saveBlogTag(Long blogId, Long tagId);
+
+	void updateBlogRecommendById(Long blogId, Boolean recommend);
+
+	void updateBlogVisibilityById(Long blogId, BlogVisibility blogVisibility);
+
+	void updateBlogTopById(Long blogId, Boolean top);
+
+	void updateViewsToRedis(Long blogId);
+
+	void updateViews(Long blogId, Integer views);
+
+	Blog getBlogById(Long id);
+
+	String getTitleByBlogId(Long id);
+
+	BlogDetail getBlogByIdAndIsPublished(Long id);
+
+	String getBlogPassword(Long blogId);
+
+	void updateBlog(top.mango.model.dto.Blog blog);
+
+	int countBlogByIsPublished();
+
+	int countBlogByCategoryId(Long categoryId);
+
+	int countBlogByTagId(Long tagId);
+
+	Boolean getCommentEnabledByBlogId(Long blogId);
+
+	Boolean getPublishedByBlogId(Long blogId);
+}
