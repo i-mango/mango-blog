8eba83f1e725eafd6467859461c4a73c982adad0
潘纪婷 <admin@manggo.cn>
2023-10-27 08:51:50 +0800

    添加LoginLogService.java文件

---
+++ mango-blog-api/src/main/java/top/mango/service/LoginLogService.java
@@ -0,0 +1,15 @@
+package top.mango.service;
+
+import org.springframework.scheduling.annotation.Async;
+import top.mango.entity.LoginLog;
+
+import java.util.List;
+
+public interface LoginLogService {
+	List<LoginLog> getLoginLogListByDate(String startDate, String endDate);
+
+	@Async
+	void saveLoginLog(LoginLog log);
+
+	void deleteLoginLogById(Long id);
+}
