d2c1d6833779ff5e5e2fd7b024a2631e2d95212d
潘纪婷 <admin@manggo.cn>
2023-11-01 16:37:42 +0800

    添加VisitorService.java文件

---
+++ mango-blog-api/src/main/java/top/mango/service/VisitorService.java
@@ -0,0 +1,22 @@
+package top.mango.service;
+
+import org.springframework.scheduling.annotation.Async;
+import top.mango.entity.Visitor;
+import top.mango.model.dto.VisitLogUuidTime;
+
+import java.util.List;
+
+public interface VisitorService {
+	List<Visitor> getVisitorListByDate(String startDate, String endDate);
+
+	List<String> getNewVisitorIpSourceByYesterday();
+
+	boolean hasUUID(String uuid);
+
+	@Async
+	void saveVisitor(Visitor visitor);
+
+	void updatePVAndLastTimeByUUID(VisitLogUuidTime dto);
+
+	void deleteVisitor(Long id, String uuid);
+}
